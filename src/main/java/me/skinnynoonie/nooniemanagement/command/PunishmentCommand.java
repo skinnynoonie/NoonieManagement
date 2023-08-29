6a235fc166e941319f91c079747cf2949d95d15c
skinnynoonie <101953182+skinnynoonie@users.noreply.github.com>
2023-08-28 22:09:57 -0400

    Todo PunishmentCommand.java

--- src/main/java/me/skinnynoonie/nooniemanagement/command/PunishmentCommand.java
+++ src/main/java/me/skinnynoonie/nooniemanagement/command/PunishmentCommand.java
@@ -3,6 +3,7 @@ package me.skinnynoonie.nooniemanagement.command;
 import me.skinnynoonie.nooniemanagement.database.ManagementDatabase;
 import org.bukkit.plugin.Plugin;
 
+// todo: Delete this class before it's too late, I feel like this class only serves the purpose of my auto completing in my ide.
 public abstract class PunishmentCommand implements CustomCommand {
 
     protected final Plugin plugin;
